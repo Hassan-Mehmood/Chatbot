274a037cde1640f44871af8eb444787bab522292
Hassan Mehmood <hassanafridi1913@gmail.com>
2024-05-10 15:12:49 +0500

    Commented the unused code

--- Rag.py
+++ Rag.py
@@ -1,4 +1,4 @@
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
@@ -6,7 +6,7 @@ from langchain_openai import OpenAIEmbeddings
 
 class Rag:
 	def __init__(self):
-		self.llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+		# self.llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
 		self.splitter = RecursiveCharacterTextSplitter(
 			separators=["\n\n", "\n", " "], 
 			chunk_size=1000, 
